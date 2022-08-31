abf00dbeb79adec0675fc37da4e70525a2984b52
Kelvin <crunnel2@jh.edu>
2022-08-31 11:04:33 -0400

    script and readme added

---
+++ day2-homework/README.md
@@ -0,0 +1,3 @@
+# QBB2022 - Day 2 - Homework Excercises Submission
+
+Exercise 2: There are 100 records that do not have a corresponding ID in dbSNP
---
+++ day2-homework/analysis_script.py
@@ -0,0 +1,33 @@
+#import vcf parser
+from vcfParser import parse_vcf
+
+#parse the two vcf files
+random = parse_vcf("random_snippet.vcf")
+dbSNP = parse_vcf("dbSNP_snippet.vcf")
+
+#initialize empty position-ID dictionary
+SNP_ID_dict = {}
+
+#loop through all the lines in dbSNP
+for i in range(len(dbSNP)):
+
+	#set the key in dict to positions and value to IDs
+	SNP_ID_dict[dbSNP[i][1]] = dbSNP[i][2]
+
+#initialize counter of non corresponding IDs
+noID = 0
+
+#loop through every line of random snippet
+for i in range(len(random)):
+	
+	#try to set the ID in random to the ID in SNP dictionary
+	#with corresponding position value
+	try:
+		random[i][2] = SNP_ID_dict[random[i][1]]
+	
+	#if there is no matching position, increment noID counter
+	except:
+		noID += 1
+
+print(f"There are {noID} records that do not have a corresponding ID in dbSNP")
+
