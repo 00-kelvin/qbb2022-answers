c2c391d1fd46782d647475fa2067f5d420b6c5cd
Kelvin <crunnel2@jh.edu>
2022-09-25 11:22:30 -0400

    successfully ran steps 1-3

--- week3-homework/hw3.sh
+++ week3-homework/hw3.sh
@@ -15,11 +15,13 @@ done
 	
 # Step 3a: create sorted bam files
 for SAMPLE in A01_09 A01_11 A01_23 A01_24 A01_27 A01_31 A01_35 A01_39 A01_62 A01_63
-	samtools sort -O bam -o ${SAMPLE}.bam ${SAMPLE}.sam
+do
+	samtools sort -@4 -O bam -o ${SAMPLE}.bam ${SAMPLE}.sam
 done
 	
 # Step 3b: create an index for each sorted bam file
 for SAMPLE in A01_09 A01_11 A01_23 A01_24 A01_27 A01_31 A01_35 A01_39 A01_62 A01_63
+do
 	samtools index ${SAMPLE}.bam
 done
 
