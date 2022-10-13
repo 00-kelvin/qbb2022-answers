72703cdc0397bbb7a432f115e926e7e5249fb438
Kelvin <crunnel2@jh.edu>
2022-10-13 17:39:26 -0400

    working on step 2

--- week5-homework/hw5.sh
+++ week5-homework/hw5.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-for FILE in D2_Sox2_R1 D2_Sox2_R1_input D2_Sox2_R2 D2_Sox2_R2_input 
+for READ in D2_Sox2_R1 D2_Sox2_R1_input D2_Sox2_R2 D2_Sox2_R2_input 
 do 
 	samtools view \
 	-q 10 \
-	-o ${FILE}_filtered.bam \
-	${FILE}.bam
+	-o ${READ}_fltrd.bam \
+	${READ}.bam
+done
+
+for REPLICATE in D2_Sox2_R1 D2_Sox2_R2
+do
+	macs2 callpeak \
+	-t ${REPLICATE}_fltrd.bam \
+	-c ${REPLICATE}_input_fltrd.bam \
+	-g 94987271 \
+	-B 
 done
