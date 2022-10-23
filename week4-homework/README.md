de9b5bd81c32aaffabc3ab7c1ca67c64689986f4
Kelvin <crunnel2@jh.edu>
2022-10-23 17:55:55 -0400

    answered question 7

--- week4-homework/README.md
+++ week4-homework/README.md
@@ -1,10 +1,33 @@
 ## Week 4 Homework
 
+#### Question 2
+
 Command to generate PC values: `plink --vcf ./gwas_data/genotypes.vcf --pca 10`
+
+#### Question 3
 
 To generate allele frequencies: `plink --vcf ./gwas_data/genotypes.vcf --freq`
 
+#### Question 4
+
 Quantitative association test: `plink --vcf ./gwas_data/genotypes.vcf --linear --pheno ./gwas_data/CB1908_IC50.txt --covar plink.eigenvec --allow-no-sex --out CB1908_IC50_gwas_results` and `plink --vcf ./gwas_data/genotypes.vcf --linear --pheno ./gwas_data/GS451_IC50.txt --covar plink.eigenvec --allow-no-sex --out GS451_IC50_gwas_results`
 
+#### Question 5
+
 For the manhattan plots, I drew a lot of inspiration from here: https://www.python-graph-gallery.com/manhattan-plot-with-matplotlib 
+
 I rewrote all the code myself and made sure I know what's going on in it but let me know if it's too close.
+
+#### Question 7
+
+**Top SNP in the CB1908 dataset: rs10876043**
+* According to the UCSC Genome Browser, this SNP may be affecting the gene DIP2B which codes for Disco-interacting protein 2 homolog B. This protein "contains a binding site for the transcriptional regulator DNA methyltransferase 1 associated protein 1 as well as AMP-binding sites" which suggests that it may be involved in DNA methylation
+* If this protein is mutated, it is possible that genes that should be methylated and thus repressed will not be. The expression of these usually repressed genes could lead to lymphocytopenia upon interaction with the drug.
+* Code to locate this SNP is in the effect_size.py script
+
+
+**Top SNP in the GS451 dataset: rs7257475**
+* This SNP appears to be in the gene ZNF826. This is a gene for a zinc finger protein, but according to the genome browser, it is a pseudogene (non-coding RNA). 
+* I am not sure how this could contribute to the disease phenotype. Perhaps the SNP somehow makes this gene active again, and it contributes to the lymphocytopenia phenotype. 
+* Code to located this SNP is in the manhattan.py script
+
--- week4-homework/manhattan.py
+++ week4-homework/manhattan.py
@@ -15,34 +15,36 @@ df_CB = df_CB[df_CB['TEST'] == 'ADD']
 df_GS = df_GS[df_GS['TEST'] == 'ADD']
 
 
-titles = ['CB1908 IC50 GWAS Results', 'GS451 IC50 GWAS Results']
+# titles = ['CB1908 IC50 GWAS Results', 'GS451 IC50 GWAS Results']
 
-for i, df in enumerate([df_CB, df_GS]):
-	df['minus_log10_pvalue'] = -np.log10(df.P)
-	df.CHR = df.CHR.astype('category')
-	df['index'] = range(len(df))
-	df_grouped = df.groupby(('CHR'))
-	df_sig = df[df['minus_log10_pvalue'] > 5]
+# for i, df in enumerate([df_CB, df_GS]):
+# 	df['minus_log10_pvalue'] = -np.log10(df.P)
+# 	df.CHR = df.CHR.astype('category')
+# 	df['index'] = range(len(df))
+# 	df_grouped = df.groupby(('CHR'))
+# 	df_sig = df[df['minus_log10_pvalue'] > 5]
 
-	fig, ax = plt.subplots(figsize=(12, 6)) 
-	colors = ['orange','gold','lightgreen', 'lightblue']
+# 	fig, ax = plt.subplots(figsize=(12, 6)) 
+# 	colors = ['orange','gold','lightgreen', 'lightblue']
 
-	x_labels = []
-	x_ticks = []
+# 	x_labels = []
+# 	x_ticks = []
 
-	for j, (name, group) in enumerate(df_grouped):
-	    group.plot(kind='scatter', x='index', y='minus_log10_pvalue',
-			color=colors[j % len(colors)], ax=ax)
-	    x_labels.append(name)
-	    x_ticks.append((group['index'].iloc[0] + (group['index'].iloc[-1] - group['index'].iloc[0])/2))
+# 	for j, (name, group) in enumerate(df_grouped):
+# 	    group.plot(kind='scatter', x='index', y='minus_log10_pvalue',
+# 			color=colors[j % len(colors)], ax=ax)
+# 	    x_labels.append(name)
+# 	    x_ticks.append((group['index'].iloc[0] + (group['index'].iloc[-1] - group['index'].iloc[0])/2))
 
-	df_sig.plot(kind='scatter', x='index', y='minus_log10_pvalue', ax=ax)
+# 	df_sig.plot(kind='scatter', x='index', y='minus_log10_pvalue', ax=ax)
 
-	ax.set_title(titles[i])
-	ax.set_xticks(x_ticks)
-	ax.set_xticklabels(x_labels, fontsize = 9)
-	ax.set_xlabel('Chromosome')
-	plt.tight_layout()
+# 	ax.set_title(titles[i])
+# 	ax.set_xticks(x_ticks)
+# 	ax.set_xticklabels(x_labels, fontsize = 9)
+# 	ax.set_xlabel('Chromosome')
+# 	plt.tight_layout()
 
-	plt.savefig(titles[i] + '.png', dpi = 100)
+# 	plt.savefig(titles[i] + '.png', dpi = 100)
 
+top_snp_gs = df_GS.loc[df_GS['P'] == df_GS['P'].min(), 'SNP'].item()
+print(top_snp_gs)
