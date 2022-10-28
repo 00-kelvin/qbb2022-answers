bec946660036ce6f70362d47135726135b3d5fc3
Kelvin <crunnel2@jh.edu>
2022-10-28 12:50:35 -0400

    format

--- week6-homework/load_data.py
+++ week6-homework/load_data.py
@@ -1,12 +1,31 @@
 #!/usr/bin/env python
 
 import sys
-
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 import seaborn as sns
 
+def remove_dd_bg(mat):
+    N = mat.shape[0]
+    mat2 = np.copy(mat)
+    for i in range(N):
+        bg = np.mean(mat[np.arange(i, N), np.arange(N - i)])
+        mat2[np.arange(i, N), np.arange(N - i)] -= bg
+        if i > 0:
+            mat2[np.arange(N - i), np.arange(i, N)] -= bg
+    return mat2
+
+def smooth_matrix(mat):
+    N = mat.shape[0]
+    invalid = np.where(mat[1:-1, 1:-1] == 0)
+    nmat = np.zeros((N - 2, N - 2), float)
+    for i in range(3):
+        for j in range(3):
+            nmat += mat[i:(N - 2 + i), j:(N - 2 + j)]
+    nmat /= 9
+    nmat[invalid] = 0
+    return nmat
 
 def main():
     # in1_fname should be ddCTCF
@@ -48,7 +67,7 @@ def main():
         j = 1000
 
         for row in fltrd:
-            row[2] = np.log10(row[2])
+            row[2] = np.log(row[2])
 
             # this is my bootleg way of finding the minimum score...
             if row[2] < j:
@@ -64,8 +83,8 @@ def main():
             row[1] = row[1] - min_bin
             row[2] = row[2] - j
 
-            # if row[2] > k:
-            #     k = row[2]
+        #     if row[2] > k:
+        #         k = row[2]
 
         # print(k)
 
@@ -80,20 +99,35 @@ def main():
             mat[row[0], row[1]] = row[2]
             mat[row[1], row[0]] = row[2]
 
-        d['mat{0}'.format(i + 1)] = mat
+        
 
         # set up my heatmap
-        sns.heatmap(mat, ax=ax[i], vmin=0, vmax=4, cmap="rocket_r", square=True,
+        sns.heatmap(mat, ax=ax[i], vmin=0, vmax=10, cmap="magma_r", square=True,
                 xticklabels=False, yticklabels=False, cbar_kws={"label": "Score"})
 
         ax[i].set_title(titles[i])
         ax[i].set_xlabel('chr15:11170245-12070245')
         ax[i].set_ylabel('chr15:11170245-12070245')
 
+        # remove distance dependent signal and smooth
+        final_mat = smooth_matrix(remove_dd_bg(mat))
+
+        # save each of my edited matrices in a dictionary for later use
+        d['mat{0}'.format(i + 1)] = final_mat
+
+    dif_mat = d['mat2'] - d['mat1']
+
+
+    sns.heatmap(dif_mat, ax=ax[2], cmap="seismic", square=True,
+                xticklabels=False, yticklabels=False, cbar_kws={"label": "Score"},
+                norm=colors.CenteredNorm())
+
+    ax[2].set_title(titles[2])
+    ax[2].set_xlabel('chr15:11170245-12070245')
+    ax[2].set_ylabel('chr15:11170245-12070245')
 
     plt.tight_layout()
-    plt.show()
-
+    plt.savefig(out_fname, dpi = 200)
 
 if __name__ == "__main__":
     main()
