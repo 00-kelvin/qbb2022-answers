00b12fa9d3d1a7544db961719bc62153327b5c37
Kelvin <crunnel2@jh.edu>
2022-11-17 18:08:29 -0500

    finished part 1

--- week9-homework/week9.py
+++ week9-homework/week9.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sb
 import numpy.lib.recfunctions as rfn
-import scipy.cluster.hierarchy as sp
+from scipy.cluster.hierarchy import linkage, leaves_list, dendrogram
 
 ############################
 ##                        ##
@@ -45,6 +47,59 @@ fpkm_fltrd_log = np.log2(fpkm_fltrd + 0.1)
 ##                 ##
 #####################
 
+gene_linkage_mtrx = linkage(fpkm_fltrd_log, 'complete')
+gene_leaves = leaves_list(gene_linkage_mtrx)
+
+sample_linkage_mtrx = linkage(fpkm_fltrd_log.T, 'complete')
+sample_leaves = leaves_list(sample_linkage_mtrx)
+
+# reorder the rows according to the gene clusters
+fpkm_row_sorted = fpkm_fltrd_log[gene_leaves]
+
+# reorder the columns according to the sample clusters
+fpkm_sorted = fpkm_row_sorted[:,sample_leaves]
+
+# reorder the column names as well
+col_names_sorted = [col_names[i] for i in sample_leaves]
+
+# make my figure...
+fig = plt.figure(figsize=(5,8))
+
+# using gridspec to get the dendrogram and heatmap to line up
+gs = fig.add_gridspec(nrows=2, ncols=2, height_ratios=[1,4], width_ratios=[5,1])
+
+# making the dendrogram first 
+ax1 = fig.add_subplot(gs[0,0])
+sample_dendr = dendrogram(sample_linkage_mtrx, ax=ax1)
+
+# no spines >:(
+for i in ax1.spines.values():
+	i.set_visible(False)
+
+# no ax ticks either
+ax1.tick_params(axis='both', which='major', labelsize=10, 
+					labelbottom=False, bottom=False, 
+					labelleft=False, left=False)
+ax1.set_xticks([])
+
+# making the heatmap 
+ax2 = fig.add_subplot(gs[1,:])
+sb.heatmap(fpkm_sorted, ax=ax2)
+
+# no spines, some ticks
+for i in ax2.spines.values():
+	i.set_visible(False)
+
+ax2.tick_params(axis='both', which='major', labelsize=10, 
+					labelbottom=False, bottom=False, 
+					labeltop=True, top=False, left=False)
+ax2.set_xticks(np.arange(len(col_names_sorted))+.5)
+ax2.set_xticklabels(col_names_sorted, rotation=90)
+ax2.set_yticklabels([])
+
+# plot!
+plt.tight_layout()
+plt.savefig('heatmap_and_dendr.png')
 
 
 #######################################
