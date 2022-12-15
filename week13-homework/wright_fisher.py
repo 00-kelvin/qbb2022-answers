b5c5e7e7f281e0667ea46fe4a6a52e90fb282654
Kelvin <crunnel2@jh.edu>
2022-12-15 18:22:38 -0500

    finished

--- week13-homework/wright_fisher.py
+++ week13-homework/wright_fisher.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib
+# import matplotlib
 
 # p is the starting allele frequency, N is the population size
 def sim_wright_fisher(p, N):
@@ -88,11 +88,17 @@ def fixation_time_vs_pop_size(N_values):
 	ax.set_ylabel("Time to fixation")
 
 	ax.set_xscale('log')
-	ax.set_xticks(N_values)
-	ax.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
-	plt.xticks(rotation=45, ha='right')
+
+	# Made another version of the figure with log-scale y-axis too
+	ax.set_yscale('log')
 
 
+	# Ended up using even values so didn't need this code
+	# ax.set_xticks(N_values)
+	# ax.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
+	# plt.xticks(rotation=45, ha='right')
+
+	# Another way of annotating the N values that I didn't end up using
 	# for N, t in zip(N_values, times):
 	# 	ax.annotate(
 	# 		"N = " + str(N),
@@ -102,13 +108,43 @@ def fixation_time_vs_pop_size(N_values):
 	ax.set_title("Time to fixation vs. population size")
 
 	plt.tight_layout()
-	plt.show()
+	plt.savefig("fixation_time_vs_pop_size_log_time.png", dpi=200)
 
+# takes a list of allele frequencies as argument
+def fixation_time_vs_allele_freq(p_values, runs, N):
+
+	data = []
+
+	for p in p_values:
+		times = []
+
+		for i in range(runs):
+			time_to_fix = len(sim_wright_fisher(p, N))
+			times.append(time_to_fix)
+
+		data.append(times)
+
+	fig, ax = plt.subplots()
+
+	# I can't figure out how to get the violin plots to not overlap. Oh well
+	ax.violinplot(data, p_values)
+	ax.set_xlabel("Starting allele frequency")
+	ax.set_ylabel("Time to fixation")
+	ax.set_title(
+		"in " + str(runs) + " simulations with a population size of " + str(N)
+		)
+	plt.suptitle("Time to fixation vs. allele frequency", weight='bold')
+	ax.set_xlim(-0.2, 1.2)
+
+	ax.set_xticks(p_values)
+
+	plt.savefig("fixation_time_vs_allele_freq.png", dpi=200)
 
 def main():
 	# allele_freq_vs_gen_number(0.3, 100)
 	# time_to_fixation_histogram(1000)
-	fixation_time_vs_pop_size([100,1000,5000,10000,50000,100000])
+	# fixation_time_vs_pop_size([100,1000,10000,100000,1000000,10000000])
+	fixation_time_vs_allele_freq(np.arange(0, 1.1, 0.1), 100, 1000)
 
 
 if __name__ == "__main__":
